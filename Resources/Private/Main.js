06ba3e85218f731b16e2e63ad64c8fb0ac2671df
Jon Uhlmann <account@uhlmann.pro>
2024-02-22 14:11:12 +0100

    New: First commit

---
+++ Resources/Private/Main.js
@@ -0,0 +1,43 @@
+import lscache from "lscache";
+
+function replaceTag(element, markup) {
+    const attributes = [...element.attributes]
+        .filter((item) => item.name != "x-data")
+        .map((item) => [item.name, item.value]);
+    const div = document.createElement("div");
+    div.innerHTML = markup;
+    const newElement = div.firstElementChild;
+    attributes.forEach(([name, value]) => {
+        newElement.setAttribute(name, value);
+    });
+    element.replaceWith(newElement);
+}
+
+window.addEventListener("alpine:init", () => {
+    window.Alpine.data("icon", (segment) => ({
+        init() {
+            lscache.setBucket("icons");
+            const cache = lscache.get(segment);
+            if (cache && typeof cache === "string") {
+                replaceTag(this.$el, cache);
+                return;
+            }
+            fetch(`/garagist/fontawesome/${segment.replaceAll(":", "/")}.svg`)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Failed to load icon");
+                    }
+                    return response.text();
+                })
+                .then((data) => {
+                    // Store for 1 week 60 min * 24 hours * 7 days = 10080
+                    lscache.set(segment, data, 10080);
+                    replaceTag(this.$el, data);
+                })
+                .catch((error) => {
+                    console.warn(error);
+                    this.$el.remove();
+                });
+        },
+    }));
+});
